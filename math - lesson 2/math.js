0ccd34a40d325c160b2f2b04e741b0ab10637dd8
ErhanGokce <erhangokceofis@gmail.com>
2025-10-02 08:05:59 +0300

    update readme

---
+++ math - lesson 2/math.js
@@ -0,0 +1,20 @@
+// console.log(Math.round(5.7));
+
+// strings strings = text
+
+//alert('hello');
+// console.log('hello')
+// console.log('hi' + ' ' +  'how are you?')
+//console.log(typeof 'hello') //string
+//console.log(typeof 23) //number
+//console.log(typeof true) //boolean
+//console.log(typeof false) //boolean
+//console.log(typeof undefined) //undefined
+//console.log(typeof null) //object
+//console.log(typeof {}) //object
+//console.log(typeof []) //object
+//console.log(Items = ['apple', 'banana', 'orange']);
+//console.log('Items (' + (1 + 1) + '): $');
+//console.log('some\ntext'); // \n alt satıra geçiriyor
+//console.log('hello'.length) //chracter leght 5
+//console.log(`hello`) // template stringsq1
