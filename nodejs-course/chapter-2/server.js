4b4587775db3c14c58735c029db8bbacb84678c1
ErhanGokce <104887152+ErhanGokce@users.noreply.github.com>
2026-01-01 08:06:45 +0300

    Feat: api endpoints and recaps. next chapter is crud operations

--- nodejs-course/chapter-2/server.js
+++ nodejs-course/chapter-2/server.js
@@ -2,16 +2,35 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+let users = [
+    {id: 1, name: 'John Doe', age: 30}
+]
+
 app.get('/', (req, res) => {
     console.log('homepage endpoint called', req.method);
+    res.send('<h1>Welcome to this website</h1>');
     res.sendStatus(200);
 })
 app.get('/dashboard', (req, res) => {
     console.log('dashboard endpoint called', req.method);
+    res.send('<h1>Welcome to this website</h1>');
+    res.sendStatus(200);
+})
+app.get('/about', (req, res) => {
     console.log(req.query);
+    res.send('<h1>Welcome to this website</h1>');
     res.sendStatus(200);
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
+//CRUD - delete and update operations
+
+//API ENDPOINTS
+app.get('/api/users', (req, res) => {
+    res.send(users);
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
